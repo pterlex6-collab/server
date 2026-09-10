8ab435d12c4547c343035693f4c721f891e16bab
pterlex6-collab <pterlex6@gmail.com>
2026-09-11 06:19:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // ================================================
-// CARD GAME NUSANTARA - FULL SERVER v2
+// CARD GAME NUSANTARA - FULL SERVER untuk Railway sever 1
 // judul skripsi : PENGEMBANGAN MEDIA WEBSITE CARD GAME NUSANTARA MATERI DAERAHKU KEBANGGAANKU PADA MATA PELAJARAN IPAS KELAS V SD
 // server.js - Converted from Deno (main.ts) to Node.js
 // ================================================
